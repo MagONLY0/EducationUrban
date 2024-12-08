a084a47e46c8cb54ec3694597e3ec9d96b811d73
MagONLY0 <maryasovalexntk@gmail.com>
2024-12-08 19:56:09 +0700

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -6,3 +6,16 @@ age = age+2
 print (age)
 is_student = True
 print (is_student)
+
+task = 12
+hour = 1.5
+education = "Python"
+Oclock_on_task = hour / task
+print("Курс: ",education, ", всего задач: ", task, ",затрачено времени: ", hour, ", среднее время выполнения ", Oclock_on_task, " часа.")
+
+a = "Топинамбур"
+print(a[0])
+print(a[-1])
+print(a[5:])
+print(a[::-1])
+print(a[1:10:2])
